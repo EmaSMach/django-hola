caddf37a676cac270d6970b8ec19c7ed04eec411
axel <denysiuk.isi@gmail.com>
2020-08-18 19:28:26 -0300

    algunos templates

--- proyecto_django/bienvenido/urls.py
+++ proyecto_django/bienvenido/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("hola/", views.index, name="index"),
     path("chau/", views.chau, name="despedida"),
+    path("empleados/", views.empleados),
     path("empleado/<int:id>", views.mostrar_empleado, name="empleado"),
     path("borrar_emp/<int:id>", views.borrar_empleado),
  
--- proyecto_django/bienvenido/views.py
+++ proyecto_django/bienvenido/views.py
@@ -5,17 +5,33 @@ from bienvenido.models import Empleado
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hola Mundo!")
+    contexto = {
+        "saludo":"Hola Mundo",
+        "numero":234
+    }
+    return render(request, "hola.html", contexto)
+    #return HttpResponse("Hola Mundo!")
 
 def chau(request):
-    return HttpResponse("Adiós Mundo!")
+    return render(request, "chau.html", {})
 
 def mostrar_empleado(request, id):
     emp = Empleado.objects.get(pk=id)
-    return HttpResponse(emp.nombre)
+    contexto = {
+        "empleado":emp
+    }
+    return render(request, "empleado.html", contexto)
+    #return HttpResponse(emp.nombre)
 
 def borrar_empleado(request, id):
     emp = Empleado.objects.get(pk=id)
     nombre = emp.nombre
     emp.delete()
     return HttpResponse(nombre + " Borrado!")
+
+def empleados(request):
+    lista_empleados = Empleado.objects.all()
+    contexto = {
+        "empleados":lista_empleados,
+    }
+    return render(request, "empleados.html", contexto)
