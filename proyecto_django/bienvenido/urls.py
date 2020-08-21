d446806097da228f0c0a39164a38910aa8ef4eda
axel <denysiuk.isi@gmail.com>
2020-08-20 22:38:16 -0300

    rediseño urls y templates

--- proyecto_django/bienvenido/urls.py
+++ proyecto_django/bienvenido/urls.py
@@ -4,14 +4,14 @@ from . import views
 urlpatterns = [
     path("hola/", views.index, name="index"),
     path("chau/", views.chau, name="despedida"),
-    path("empleados/", views.empleados),
-    path("detalle/<int:id>", views.mostrar_empleado, name="empleado"),
-    path("borrar_emp/<int:id>", views.borrar_empleado, name="borrar_empleado"),
-    path("departamentos/", views.listar_departamentos, name="departamentos_lista"),
-
     path("base/", views.base_template),
-    path("nuevo_empleado/", views.crear_empleado),
 
-    path("nuevo_departamento/", views.crear_departamento),    
+    path("empleados/", views.empleados),
+    path("empleado/<int:id>", views.mostrar_empleado, name="empleado"),
+    path("empleado/nuevo/", views.crear_empleado),
+    path("empleado/borrar/<int:id>", views.borrar_empleado, name="borrar_empleado"),
+
+    path("departamentos/", views.listar_departamentos, name="departamentos_lista"),
+    path("departamento/nuevo/", views.crear_departamento),    
 
 ]
