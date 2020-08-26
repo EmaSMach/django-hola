796f517fad8635ebb3dfd6b8ee10c2cad8e680f1
axel <denysiuk.isi@gmail.com>
2020-08-26 20:18:53 -0300

    ejemplo agregar clases form fields

--- proyecto_django/bienvenido/forms.py
+++ proyecto_django/bienvenido/forms.py
@@ -8,6 +8,10 @@ class EmpleadoForm(forms.ModelForm):
         model = Empleado
         fields = "__all__"
 
+    def __init__(self, *args, **kwargs):
+        super(EmpleadoForm, self).__init__(*args, **kwargs)
+        self.fields['nombre'].widget.attrs.update({'class' : 'texto-rojo','placeholder' : 'ingrese su nombre', 'type' : 'text'})
+
 class DepartamentoForm(forms.ModelForm):
     class Meta:
         model=Departamento
